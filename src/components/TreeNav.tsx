a40350e3f8df1815a0cd4ecade50a470346dac3a
Minjoo Cho <minjoolisa.cho@gmail.com>
2023-09-04 22:49:05 +0100

    collapsible tree

--- src/components/TreeNav.tsx
+++ src/components/TreeNav.tsx
@@ -12,10 +12,10 @@ const height = 600;
 
 const TreeNav = ({ data }: { data: TreeNode }) => {
   const [dimension, setDimension] = useState({ w: width, h: height });
-  const [tree, setTree] = useState(data);
-  const ref = useRef<SVGSVGElement>(null);
+  const [root, setRoot] = useState(d3.hierarchy(data));
 
-  //console.log(data);
+  const linesRef = useRef<SVGGElement>(null);
+  const nodesRef = useRef<SVGGElement>(null);
 
   const drag = useCallback(
     (simulation: d3.Simulation<d3.SimulationNodeDatum, undefined>) => {
@@ -46,92 +46,95 @@ const TreeNav = ({ data }: { data: TreeNode }) => {
   );
 
   const update = useCallback(
-    (
-      root: d3.HierarchyNode<TreeNode>,
-      links: d3.HierarchyLink<TreeNode>[]
-    ) => {},
-    []
-  );
-  const flatten = useCallback((root: TreeNode) => {
-    let nodes: TreeNode[] = [],
-      i = 0;
+    (root: d3.HierarchyNode<TreeNode>) => {
+      const links = root.links();
+      const nodes = root.descendants();
 
-    const recurse = (node: any) => {
-      if (node.children) node.children.forEach(recurse);
-      if (!node.id) node.id = ++i;
-      nodes.push(node);
-    };
-    recurse(root);
-    return nodes;
-  }, []);
+      const simulation = d3
+        .forceSimulation(nodes as d3.SimulationNodeDatum[])
+        .force(
+          "link",
+          d3
+            .forceLink(links as SimulationLinkDatum<SimulationNodeDatum>[])
+            .id((d: any) => d.id)
+            .distance(0)
+            .strength(1)
+        )
+        .force("charge", d3.forceManyBody().strength(-50))
+        .force("x", d3.forceX())
+        .force("y", d3.forceY());
+
+      // remove old lines
+      d3.select(linesRef.current!).selectAll("line").remove();
+
+      const link = d3
+        .select(linesRef.current!)
+        .selectAll("line")
+        .data(links)
+        .join("line");
+
+      // remove old nodes
+      d3.select(nodesRef.current!).selectAll("circle").remove();
+
+      const node = d3
+        .select(nodesRef.current!)
+        .selectAll("circle")
+        .data(nodes)
+        .join("circle")
+        .attr("fill", (d: any) => (d.children ? null : "#000"))
+        .attr("stroke", (d: any) => (d.children ? null : "#fff"))
+        .attr("r", 3.5)
+        .on("click", (event: any, d: any) => {
+          if (!event.defaultPrevented) {
+            //console.log(d.children);
+            if (d.children) {
+              d._children = d.children;
+              d.children = null;
+            } else {
+              d.children = d._children;
+              d._children = null;
+            }
+
+            update(root);
+          }
+        })
+        .call(drag(simulation) as any);
+
+      node.append("title").text((d: any) => d.data.name);
+
+      // Exit any old nodes.
+      node.exit().remove();
+
+      simulation.on("tick", () => {
+        link
+          .attr("x1", (d: any) => d.source.x)
+          .attr("y1", (d: any) => d.source.y)
+          .attr("x2", (d: any) => d.target.x)
+          .attr("y2", (d: any) => d.target.y);
+
+        node.attr("cx", (d: any) => d.x).attr("cy", (d: any) => d.y);
+      });
+    },
+    [drag]
+  );
 
   useEffect(() => {
-    const root = d3.hierarchy(tree);
-    const links = root.links();
-    const nodes = root.descendants();
+    update(root);
+  }, [update, root]);
 
-    const simulation = d3
-      .forceSimulation(nodes as d3.SimulationNodeDatum[])
-      .force(
-        "link",
-        d3
-          .forceLink(links as SimulationLinkDatum<SimulationNodeDatum>[])
-          .id((d: any) => d.id)
-          .distance(0)
-          .strength(1)
-      )
-      .force("charge", d3.forceManyBody().strength(-50))
-      .force("x", d3.forceX())
-      .force("y", d3.forceY());
-
-    const svg = d3.select(ref.current!);
-
-    // Append links.
-    const link = svg
-      .append("g")
-      .attr("stroke", "#999")
-      .attr("stroke-opacity", 0.6)
-      .selectAll("line")
-      .data(links)
-      .join("line");
-
-    // Append nodes.
-    const node = svg
-      .append("g")
-      .attr("fill", "#fff")
-      .attr("stroke", "#000")
-      .attr("stroke-width", 1.5)
-      .selectAll("circle")
-      .data(nodes)
-      .join("circle")
-      .attr("fill", (d: any) => (d.children ? null : "#000"))
-      .attr("stroke", (d: any) => (d.children ? null : "#fff"))
-      .attr("r", 3.5)
-      .call(drag(simulation) as any);
-
-    node.append("title").text((d: any) => d.data.name);
-
-    simulation.on("tick", () => {
-      link
-        .attr("x1", (d: any) => d.source.x)
-        .attr("y1", (d: any) => d.source.y)
-        .attr("x2", (d: any) => d.target.x)
-        .attr("y2", (d: any) => d.target.y);
-
-      node.attr("cx", (d: any) => d.x).attr("cy", (d: any) => d.y);
-    });
-  }, [drag, tree]);
   return (
     <div className={c.svgContainer}>
       <svg
-        ref={ref}
         width={dimension.w}
         height={dimension.h}
         viewBox={`${-dimension.w / 2} ${-dimension.h / 2} ${dimension.w} ${
           dimension.h
         }`}
         className={c.svgComponent}
-      ></svg>
+      >
+        <g ref={linesRef} style={{ stroke: "#999", strokeOpacity: 0.6 }}></g>
+        <g ref={nodesRef} style={{ fill: "#fff", stroke: "#fff" }}></g>
+      </svg>
     </div>
   );
 };
