2a5c816af441e30ca2f0125d1008c46d49349a4d
Minjoo Cho <minjoolisa.cho@gmail.com>
2023-09-05 17:30:41 +0100

    font import, error fix

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,14 +1,15 @@
 "use client";
 import c from "./MainNav.module.scss";
+import { orator } from "@/styles/fonts";
 
 const MainNav = () => {
   return (
     <div className={c.nav}>
-      <button className={c.button}>
+      <button className={`${c.button} ${orator.className}`}>
         <a href="http://minjoocho.com">Back to Home</a>
       </button>
       <h1 className={c.title}>
-        Minjoo&#x27;s Experiment<span>(WIP)</span>
+        Minjoo&#x27;s Experiment<div className={c.wip}>(WIP)</div>
       </h1>
       <p className={c.description}>
         This is (going to be) showcasing Minjoo&#x27;s various technical
---
+++ src/styles/fonts.ts
@@ -0,0 +1,11 @@
+import localFont from "next/font/local";
+
+const orator = localFont({
+  src: [
+    {
+      path: "./fonts/Orator-Std-Font.otf",
+    },
+  ],
+});
+
+export { orator };
