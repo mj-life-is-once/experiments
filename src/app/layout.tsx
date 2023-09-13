2c2c03e1daffcc73e54431c1ba1851c53f4a013a
Minjoo Cho <minjoolisa.cho@gmail.com>
2023-09-14 00:04:11 +0100

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-1SWH4LC4SH" />
+      <Script id="google-analytics">
+        {`  window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1SWH4LC4SH');`}
+      </Script>
       <body className={`${inter.className}`}>{children}</body>
     </html>
   );
